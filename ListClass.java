b0fea3b76d92f36dfe0cc00a5bf68fc78ab7e989
Miangaly5 <nancyrabezakarison@gmail.com>
2024-11-13 13:46:37 +0100

    Sprint 8

--- ListClass.java
+++ ListClass.java
@@ -5,13 +5,17 @@ import annotation.Param;
 import com.thoughtworks.paranamer.AdaptiveParanamer;
 import com.thoughtworks.paranamer.Paranamer;
 
-import java.util.*;
 import java.net.URL;
 import java.io.File;
 import java.io.IOException;
+import java.util.Map;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
 import java.lang.annotation.Annotation;
+import jakarta.servlet.http.HttpSession;
 import jakarta.servlet.http.HttpServletRequest;
 
 public class ListClass {
@@ -91,6 +95,132 @@ public class ListClass {
             parameterValues.add(value);
         }
         
+        return parameterValues;
+    }
+
+    //Pour les arguments de type Object
+    public static ArrayList<Object> getParameterValuesForObjects(Method method, HttpServletRequest request) throws Exception {
+        ArrayList<Object> parameterValues = new ArrayList<>();
+        Paranamer paranamer = new AdaptiveParanamer();
+        String[] parameterNamesArray = paranamer.lookupParameterNames(method, false);
+        Parameter[] parameters = method.getParameters();
+
+        for (int i = 0; i < parameters.length; i++) {
+            int index = i;
+            Parameter parameter = parameters[i];
+            Class<?> paramType = parameter.getType();
+
+            if (!paramType.isPrimitive() && paramType != String.class) {
+                @SuppressWarnings("deprecation")
+                Object paramObject = paramType.newInstance();
+
+                Map<String, String[]> parameterMap = request.getParameterMap().entrySet().stream()
+                        .filter(entry -> entry.getKey().startsWith(parameterNamesArray[index] + "."))
+                        .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+
+                for (Map.Entry<String, String[]> entry : parameterMap.entrySet()) {
+                    String key = entry.getKey();
+                    String attributeName = key.substring(key.lastIndexOf(".") + 1);
+                    String value = entry.getValue()[0];
+                    try {
+                        Field field = paramType.getDeclaredField(attributeName);
+                        field.setAccessible(true);
+                        field.set(paramObject, convertValue(field.getType(), value));
+                    } catch (Exception e) {
+                        throw new IllegalArgumentException("Impossible de définir la valeur du paramètre " + key, e);
+                    }
+                }
+                parameterValues.add(paramObject);
+            }
+            else {
+                throw new IllegalArgumentException("Le paramètre " + parameter.getName() + " n'est pas un objet.");
+            }
+        }
+        return parameterValues;
+    }
+
+    private static Object convertValue(Class<?> type, String value) {
+        if (type.equals(Integer.class) || type.equals(int.class)) {
+            return Integer.valueOf(value);
+        }
+        else if (type.equals(Long.class) || type.equals(long.class)) {
+            return Long.valueOf(value);
+        }
+        else if (type.equals(Double.class) || type.equals(double.class)) {
+            return Double.valueOf(value);
+        }
+        else if (type.equals(Boolean.class) || type.equals(boolean.class)) {
+            return Boolean.valueOf(value);
+        }
+        else {
+            return value;
+        }
+    }
+
+    public static ArrayList<Object> getParameterValuesCombined(Method method, HttpServletRequest request) throws Exception {
+        ArrayList<Object> parameterValues = new ArrayList<>();
+        Paranamer paranamer = new AdaptiveParanamer();
+        String[] parameterNamesArray = paranamer.lookupParameterNames(method, false);    
+        Parameter[] parameters = method.getParameters();
+
+        for (int i = 0; i < parameters.length; i++) {
+            int index = i;
+            Parameter parameter = parameters[i];
+            Class<?> paramType = parameter.getType();
+
+            if (paramType == MySession.class) {
+                HttpSession session = request.getSession();
+                MySession mySession = new MySession(session);
+                parameterValues.add(mySession);
+            }
+            else if (!paramType.isPrimitive() && paramType != String.class) {
+                @SuppressWarnings("deprecation")
+                Object paramObject = paramType.newInstance();    
+                Map<String, String[]> parameterMap = request.getParameterMap().entrySet().stream()
+                        .filter(entry -> entry.getKey().startsWith(parameterNamesArray[index] + "."))
+                        .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+    
+                for (Map.Entry<String, String[]> entry : parameterMap.entrySet()) {
+                    String key = entry.getKey();
+                    String attributeName = key.substring(key.lastIndexOf(".") + 1);
+                    String value = entry.getValue()[0];    
+                    try {
+                        Field field = paramType.getDeclaredField(attributeName);
+                        field.setAccessible(true);
+                        field.set(paramObject, convertValue(field.getType(), value));
+                    } catch (Exception e) {
+                        throw new IllegalArgumentException("Impossible de définir la valeur du paramètre " + key, e);
+                    }
+                }    
+                parameterValues.add(paramObject);
+            }
+            else {
+                String value = null;
+                if (parameter.isAnnotationPresent(Param.class)) {
+                    Param argument = parameter.getAnnotation(Param.class);
+                    String arg_name = argument.value();
+                    value = request.getParameter(arg_name);
+                } else {
+                    String paramName = parameterNamesArray[i];
+                    String[] requestParamNames = request.getParameterMap().keySet().toArray(new String[0]);
+                    boolean found = false;
+                    for (String requestParamName : requestParamNames) {
+                        if (requestParamName.equals(paramName)) {
+                            found = true;
+                            value = request.getParameter(requestParamName);
+                            break;
+                        }
+                    }
+                    if (!found) {
+                        value ="null";
+                    }
+                }
+                if (value == null) {
+                    throw new IllegalArgumentException("Paramètre manquant ou invalide: " + parameter.getName());
+                }
+                parameterValues.add(value);
+            }
+        }
         return parameterValues;
     }
 }
