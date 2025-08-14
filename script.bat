a84571c3cd16eb74660169bf53b3fecbfa8bf34b
Miangaly5 <nancyrabezakarison@gmail.com>
2025-08-14 22:33:53 +0300

    modification-structure

--- script.bat
+++ script.bat
@@ -1,25 +1,35 @@
-mkdir "mybin"
-set mybin="D:\DOCUMENT_NANCY\S4\Mr Naina\Sprint\fw\mybin"
-set ref="D:\DOCUMENT_NANCY\S4\Mr Naina\Sprint\fw\lib\*"
+@REM Définition des chemins existants
+@REM Les fichiers compilés
+set bin-dir="D:\Documents\S4\Mr Naina\Sprint\Sprint_Nouveau\fw\bin"
+@REM Librairies nécessaires
+set lib-dir="D:\Documents\S4\Mr Naina\Sprint\Sprint_Nouveau\fw\lib\*"
+@REM Codes source du framework
+set src-dir="D:\Documents\S4\Mr Naina\Sprint\Sprint_Nouveau\fw\src"
+@REM Librairies de l'utilisateur
+set target-lib-dir="D:\Documents\S4\Mr Naina\Sprint\Sprint_Nouveau\Test\lib"
 
-@REM Compilation des fichiers dans le répertoire src et ses sous-répertoires
-for /r ".\src" %%f in (*.java) do (
-   copy "%%f" "mybin\%%~nf.java"
-)
-cd mybin
-javac -cp %ref% -d "../mybin" *.java
-for /r "." %%f in (*.java) do (
-   del "%%f" 
-)
-cd .. 
+@REM Création d'un dossier temporaire pour stocker les fichiers .java
+mkdir temp
+set temp="D:\Documents\S4\Mr Naina\Sprint\Sprint_Nouveau\fw\temp"
 
-@REM Définition des chemins
-set bin="D:\DOCUMENT_NANCY\S4\Mr Naina\Sprint\fw\bin"
-set mylib="D:\DOCUMENT_NANCY\S4\Mr Naina\Sprint\Test\lib"
-set jar=sprint0.jar
+@REM Copie des fichiers .java dans un même répertoire
+for /r %src-dir% %%f in (*.java) do (
+   copy "%%f" %temp%
+)
+
+@REM Compilation des fichiers .java et les stockés dans le dossier classes
+javac -parameters -cp %lib-dir% -d %bin-dir% %temp%\*.java
 
 @REM Création du fichier JAR
-jar -cvf %jar% -C %mybin% .
+set jar=sprint0.jar
+jar -cvf %jar% -C %bin-dir% .
 
-echo D | xcopy /q/y %jar% %mylib%
+@REM Copie des librairies nécessaire pour le côté utilisateur
+echo D | xcopy /q/y %lib-dir% %target-lib-dir%
+echo D | xcopy /q/y %jar% %target-lib-dir%
+
+@REM Suppression du dossier temporaire
+rmdir /q/s "temp"
+
+@REM Fin du script
 @echo Done
