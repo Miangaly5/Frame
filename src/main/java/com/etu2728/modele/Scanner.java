9364dffba4098fc7971d303940e8ea7d3f632b00
Miangaly5 <nancyrabezakarison@gmail.com>
2025-08-15 23:20:29 +0300

    modification_Sprint2

---
+++ src/main/java/com/etu2728/modele/Scanner.java
@@ -0,0 +1,52 @@
+package main.java.com.etu2728.modele;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.annotation.Annotation;
+import java.net.URL;
+import java.util.ArrayList;
+
+public class Scanner {
+
+    public static ArrayList<Class<?>> getAllClasses(String packageName) throws ClassNotFoundException, IOException {
+        ArrayList<Class<?>> classes = new ArrayList<>();
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        String path = packageName.replace('.', '/');
+
+        URL resource = classLoader.getResource(path);
+
+        if (resource == null) {
+            return classes;
+        }
+
+        File packageDir = new File(resource.getFile().replace("%20", " "));
+
+        for (File file : packageDir.listFiles()) {
+            if (file.isDirectory()) {
+                classes.addAll(Scanner.getAllClasses(packageName + "." + file.getName()));
+            } else if (file.getName().endsWith(".class")) {
+                String className = packageName + "." + file.getName().substring(0, file.getName().length() - 6);
+                classes.add(Class.forName(className));
+            }
+        }
+
+        return classes;
+    }
+
+    public static ArrayList<Class<?>> getControllerClasses(String packageName,
+            Class<? extends Annotation> annotationController)
+            throws ClassNotFoundException, IOException {
+        ArrayList<Class<?>> classes = getAllClasses(packageName);
+
+        ArrayList<Class<?>> result = new ArrayList<Class<?>>();
+
+        for (Class<?> classe : classes) {
+            if (classe.isAnnotationPresent(annotationController)) {
+                result.add(classe);
+            }
+        }
+
+        return result;
+    }
+
+}
