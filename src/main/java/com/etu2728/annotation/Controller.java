bfc09b1269b31e4614a045f3f2881e49b15c83fa
Miangaly5 <nancyrabezakarison@gmail.com>
2025-08-16 08:29:59 +0300

    suppression_dossier

--- src/main/java/com/etu2728/annotation/Controller.java
+++
@@ -1,9 +0,0 @@
-package main.java.com.etu2728.annotation;
-
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Retention;
-
-@Retention(RetentionPolicy.RUNTIME)
-public @interface Controller {
-
-}
